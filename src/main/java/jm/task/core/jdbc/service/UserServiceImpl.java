4ef54f0d5f5b5367bc0127f5d6c2c32c169492d6
andreyb34rus <andreyb34rus@gmail.com>
2022-01-20 09:34:28 +0300

    do creating UserDaoHibernateImpl instance only once in class field

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -7,16 +8,18 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    private static final UserDao USER_DAO = new UserDaoHibernateImpl();
+
     public void createUsersTable() {
-        new UserDaoHibernateImpl().createUsersTable();
+        USER_DAO.createUsersTable();
     }
 
     public void dropUsersTable() {
-        new UserDaoHibernateImpl().dropUsersTable();
+        USER_DAO.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        new UserDaoHibernateImpl().saveUser(name, lastName, age);
+        USER_DAO.saveUser(name, lastName, age);
         System.out.printf("User с именем – %s добавлен в базу данных.\n", name);
     }
 
@@ -25,7 +28,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new UserDaoHibernateImpl().getAllUsers();
+        List<User> userList = USER_DAO.getAllUsers();
         for (User user : userList) {
             System.out.println(user);
         }
@@ -33,6 +36,6 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-        new UserDaoHibernateImpl().cleanUsersTable();
+        USER_DAO.cleanUsersTable();
     }
 }
