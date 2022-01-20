cf9f03d8e0131cb0a3e953d745706dacaaed8f44
andreyb34rus <andreyb34rus@gmail.com>
2022-01-20 09:38:54 +0300

    change userService var to UserService interface type

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserServiceImpl UserServiceImpl = new UserServiceImpl();
-        UserServiceImpl.createUsersTable();
-        UserServiceImpl.saveUser("Erich", "Gamma", (byte) 33);
-        UserServiceImpl.saveUser("Richard", "Helm", (byte) 35);
-        UserServiceImpl.saveUser("Ralph", "Johnson", (byte) 39);
-        UserServiceImpl.saveUser("John", "Vlissides", (byte) 33);
-        UserServiceImpl.getAllUsers();
-        UserServiceImpl.cleanUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Erich", "Gamma", (byte) 33);
+        userService.saveUser("Richard", "Helm", (byte) 35);
+        userService.saveUser("Ralph", "Johnson", (byte) 39);
+        userService.saveUser("John", "Vlissides", (byte) 33);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
     }
 }
